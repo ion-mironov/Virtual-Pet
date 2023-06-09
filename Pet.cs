ddd0f8e5495a683a73c79bd2286f94fdbd48b110
Ivon Mironov <mironov.ion@gmail.com>
2023-06-09 14:21:16 -0400

    First commit with creating Pet Class.

--- Pet.cs
+++ Pet.cs
@@ -1,13 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace template_csharp_virtual_pet
+﻿namespace template_csharp_virtual_pet
 {
     public class Pet
     {
+        // FIELDS //
+        private string petName;
+        private string petSpecies;
+        private int petHunger;
+        private int petBoredom;
+        private int PetHealth;
 
+        // PROPERTIES //
+        public string PetName;
+        public string PetSpecies;
+        public int PetHunger;
+        public int PetBoredom;
+        public int petHealth;
+
+        // CONSTRUCTOR //
+
+
+        // METHODS //
+
+        //public void SomeMethodName()
+        //{
+
+        //}
     }
 }
