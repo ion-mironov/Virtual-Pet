792b36b879b96a3553570a717238bb86fe93107f
Ivon Mironov <mironov.ion@gmail.com>
2023-06-09 14:25:30 -0400

    Second attempt at initial commit.

--- Pet.cs
+++ Pet.cs
@@ -8,6 +8,21 @@ namespace template_csharp_virtual_pet
 {
     public class Pet
     {
+        // FIELDS //
+        private string petName;
+        private string petSpecies;
+        private int petHunger;
+        private int petBoredom;
+        private int PetHealth;
+
+        // PROPERTIES //
+        public string PetName { get; set; }
+        public string PetSpecies { get; set; }
+        public int PetHunger { get; set; }
+        public int PetBoredom { get; set; }
+        public int petHealth { get; set; }
+
+        // CONSTRUCTOR //
 
     }
 }
